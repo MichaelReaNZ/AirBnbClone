b6a392b6a16fdd151584bb365c3daaacd1da1470
MichaelReaNZ <MichaelReaNZ@gmail.com>
2023-05-07 16:41:56 +1200

    Category boxes

--- airbnb-clone/app/components/navbar/Navbar.tsx
+++ airbnb-clone/app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -33,6 +34,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
             <UserMenu currentUser={currentUser} />
           </div>
         </Container>
+        <Categories />
       </div>
     </div>
   );
